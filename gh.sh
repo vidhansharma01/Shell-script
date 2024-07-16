b1230f36f1b75bfeb06b3c3e5df0510fc3bafae6
Vidhan <vidhansharma01@gmail.com>
2024-07-16 23:38:05 +0530

    some shell commands

---
+++ gh.sh
@@ -0,0 +1,8 @@
+#! /bin/bash
+
+max=10
+for (( i=1; i <= $max; ++i ))
+do
+    echo "$i"
+done
+	
---
+++ ifelseif.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+DESTINATION=$1
+read -p "enter prefix: " prefix
+if test 12 -gt 10
+then
+	echo '12 is greater than 10'
+else
+	echo 'not true'
+fi
